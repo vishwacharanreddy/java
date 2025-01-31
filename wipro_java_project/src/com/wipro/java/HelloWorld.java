2127056f0ef141ae873a7df9a321aaf9f5e5d786
vishwacharanreddy <kondapuramvishwacharanreddy@gmail.com>
2025-01-31 17:40:39 +0530

    commit

---
+++ wipro_java_project/src/com/wipro/java/HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
