c456c125c93f28024d03a0699de7c6823d66e184
vishwacharanreddy <kondapuramvishwacharanreddy@gmail.com>
2025-02-15 17:30:31 +0530

    Day 13

---
+++ wipro_java_project/src/com/wipro/java/hashmap/Change3.java
@@ -0,0 +1,18 @@
+package com.wipro.java.hashmap;
+
+import java.util.HashMap;
+
+class Change3 {
+  public static void main(String[] args) {
+
+    HashMap<Integer, String> languages = new HashMap<>();
+    languages.put(1, "Java");
+    languages.put(2, "Python");
+    languages.put(3, "JavaScript");
+    System.out.println("Original HashMap: " + languages);
+
+    // change element with key 2
+    languages.replace(2, "C++");
+    System.out.println("HashMap using replace(): " + languages);
+  }
+}
